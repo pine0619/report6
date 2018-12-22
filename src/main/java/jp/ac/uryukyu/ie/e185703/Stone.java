9d06ac1d7d51c12a6ec2967c8585547a9a975246
松岡　隆斗 <e185703@ie.u-ryukyu.ac.jp>
2018-12-23 03:34:26 +0900

    README.mdとstoneクラスを作った。

---
+++ src/main/java/jp/ac/uryukyu/ie/e185703/Stone.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e185703;
+
+
+public class Stone {
+    int x;
+    int y;
+    String color;//B:黒, W:白, E:空
+
+    public Stone(int x, int y){
+        this.x = x;
+        this.y = y;
+        color = "E";
+    }
+
+}
