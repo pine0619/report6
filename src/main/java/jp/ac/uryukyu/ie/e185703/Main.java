c7118af85e2e9201fe8e8e529b955aa7f4f84b2e
松岡　隆斗 <e185703@ie.u-ryukyu.ac.jp>
2018-12-24 02:41:51 +0900

    初期盤面の生成と出力

---
+++ src/main/java/jp/ac/uryukyu/ie/e185703/Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e185703;
+
+public class Main {
+    public static void main(String args[]){
+
+        Field field = new Field();
+
+        field.print_board();
+    }
+
+}
--- src/main/java/jp/ac/uryukyu/ie/e185703/Stone.java
+++ src/main/java/jp/ac/uryukyu/ie/e185703/Stone.java
@@ -4,12 +4,33 @@ package jp.ac.uryukyu.ie.e185703;
 public class Stone {
     int x;
     int y;
-    String color;//B:黒, W:白, E:空
+    private String color;//B:黒, W:白, E:空
 
+    /**
+     * コンストラクタ。石の座標と色(空を表す"E")を指定する。
+     * @param x　x座標
+     * @param y　y座標
+     */
     public Stone(int x, int y){
         this.x = x;
         this.y = y;
         color = "E";
+    }
+
+    /**
+     * 石の色を指定するメソッド
+     * @param color　変えたい色(B:黒, W:白, E:空)
+     */
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    /**
+     * 石の色を返すメソッド
+     * @return 石の色
+     */
+    public String getColor(){
+        return color;
     }
 
 }
